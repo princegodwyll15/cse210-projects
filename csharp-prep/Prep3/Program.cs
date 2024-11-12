3a5fd08aface4def38ba30ee9be8d287574da0c0
princegodwyll15 <godwyllprince@mail.com>
2024-11-12 19:33:35 +0000

    update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,52 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,6);
+
+        Console.WriteLine("Welcome to the number guessing game!");
+        Console.WriteLine("Your Guess must be from 1 and 6");
+        Console.WriteLine("Would you like to play the game");
+        Console.WriteLine("Input yes or y to start!");
+        string start = Console.ReadLine();
+        if(start == "yes" || start =="y")
+        {
+           int gameCount = 0;
+           int numOfTries = 0;
+           while(gameCount <= 5)
+           {
+            Console.WriteLine("Input your guessed number here ");
+            string guessedNumber  = Console.ReadLine();
+            int guessedNumberToInt = int.Parse(guessedNumber);
+            if(guessedNumberToInt == randomNumber)
+            {
+                Console.WriteLine($"Your guess was right.The hidden number is: {randomNumber}");
+                break;
+            }
+            else if(guessedNumberToInt > randomNumber)
+            {
+                Console.WriteLine("Your guessed number is greater than the hidden number");
+            }
+            else if(guessedNumberToInt < randomNumber)
+            {
+                Console.WriteLine("Your guessed number is less than the hidden number");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number to play the game");
+            }
+            if(gameCount == 5)
+            {
+                Console.WriteLine("You have exceeded your tries, come back to play next time.Game Over");
+            }
+            gameCount++;
+            };
+        }
+        else
+        {
+            Console.WriteLine("Game over input invalid.");
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+    Console.WriteLine("A program to ask user for multiple values to find their total, average and The highest number inputted");
+    Console.WriteLine("Enter numbers from 1 upwards and 0 to terminate to find their average, sum and max of the numbers you inputted");
+    
+    List<int> numbers = new List<int>();
+    int numCount = -1;
+    while(numCount != 0){
+        Console.WriteLine("Enter a number: ");
+        string userInput = Console.ReadLine();
+        numCount = int.Parse(userInput);
+
+        if(numCount != 0){
+             numbers.Add(numCount);
+        }
+    }
+
+        int total = 0;
+        foreach(int eachNumber in numbers){
+            total += eachNumber;
+        }    
+        Console.WriteLine($"The sum of all the numbers you inuptted is: {total}");
+        float averageOfNumbers = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average of the numbers you inputted is : {averageOfNumbers}");
+
+        int maxNumber = numbers[0];
+        foreach(int number in numbers){
+            if (number > maxNumber){
+                maxNumber = number;
+            }
+        }
+        Console.WriteLine($"The maximum number of the numbers you entered is: {maxNumber}");
+
     }
 }
