27b04b6cd7252bbce8817fd8c9f922790472f762
princegodwyll15 <godwyllprince@mail.com>
2024-11-30 16:23:58 +0000

    update

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 
 class Program{
     private static void Main (string[] Args){
-Address address = new Address("Okai Mensah Street", "Accra", "Greater Accra Region", "Ghana");
+        //building neccessary constructors
+        Address address = new Address("Okai Mensah Street", "Accra", "Greater Accra Region", "Ghana");
         Customer customer = new Customer("Prince Godwyll", address);
         Product product = new Product("cyber Truck", "000121", 560000, 3);
         Order order = new Order(customer);
-        customer.SetCustomerName("Prince Godwyll");
-        customer.SetCustomerAddress(address);
         order.AddProduct(product);
-        Console.WriteLine(address.SetAddress());
+
+        //set the neccssary for the customer instance
+        customer.SetCustomerName("Prince Godwyll");
+        customer.SetCustomerAddress(address);  
+        Console.WriteLine($"HI Mr. {customer.GetCustomerName()}");
+
+        //print shipping and packing label to screen
+        Console.WriteLine($"Your packaing label for your package is:\n {order.PackingLabel()}");
+        Console.WriteLine($"Your Shipping label for your package is:\n {order.GetShippingLabel()}");
+        //print address of customer to screen
+        Console.WriteLine($"Your Address:\n{address.SetAddress()}");
+
+        //compute the total with neccessary conditions
         if (customer.GetCountry("Ghana"))
         {
             int shippingPriceForUSA = 5;
