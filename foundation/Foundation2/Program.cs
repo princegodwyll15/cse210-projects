1dc7dc5fcd728a5e8de205aa46ed7a46b3f080a0
princegodwyll15 <godwyllprince@mail.com>
2024-11-30 16:12:23 +0000

    update

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -1,24 +1,25 @@
 using System;
 using System.Collections.Generic;
+
 class Program{
-    static void main (string[] Args){
-        Address address = new Address("Okai Mensah Street", "Accra", "Greater Accra Region", "Ghana");
+    private static void Main (string[] Args){
+Address address = new Address("Okai Mensah Street", "Accra", "Greater Accra Region", "Ghana");
         Customer customer = new Customer("Prince Godwyll", address);
         Product product = new Product("cyber Truck", "000121", 560000, 3);
         Order order = new Order(customer);
-
         customer.SetCustomerName("Prince Godwyll");
         customer.SetCustomerAddress(address);
-
         order.AddProduct(product);
-
-
-        address.SetAddress();
-        //compute total cost
-        
         Console.WriteLine(address.SetAddress());
-        if(customer.GetCountry("Ghana") == true){
-            
+        if (customer.GetCountry("Ghana"))
+        {
+            int shippingPriceForUSA = 5;
+            double actTotalCost = order.TotalCost() + shippingPriceForUSA;
+            Console.WriteLine($"The Total Cost of the item ordered including Shipping fee is {actTotalCost}");
+        }
+        else
+        {
+            Console.WriteLine(order.TotalCost());
         }
     }
 }
