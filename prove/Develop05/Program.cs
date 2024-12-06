400a2c2afb42f32c7749f5a13e8de07ffa237df9
princegodwyll15 <godwyllprince@mail.com>
2024-12-06 17:39:20 +0000

    update

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop05 World!");
+    {   
+        while(true){
+        ChooseActivity();
+        string activity = Console.ReadLine().ToLower();
+
+        switch (activity)
+        {
+            case "1":
+            case "Breathing Activity":
+                ChooseActivity();
+                break;
+            case "2":
+            case "Listing Activity":
+                ChooseActivity();
+                break;
+            case "3":
+            case "Reflecting Activity":
+                ChooseActivity();
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please choose a valid option.");
+                break;
+        }
+        }
+        static void ChooseActivity(){
+        Console.WriteLine("Please Choose an Activity to begin with: ");
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("1. Breathing Activity");
+        Console.WriteLine("2. Listing Activity");
+        Console.WriteLine("3. Reflecting Activity");
+        Console.WriteLine("4. Quit");
+
+        }
     }
 }
