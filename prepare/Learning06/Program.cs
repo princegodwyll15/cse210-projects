ac8cf37070b2f24072f87e946682720c37da8e9c
princegodwyll15 <godwyllprince@mail.com>
2024-12-25 09:55:22 +0000

    update

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,23 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        int[] nums = new int[] { 1, 2, 4, 5, 6 };
+        int sum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+
+        // Print the cumulative sum array
+        foreach (int num in nums)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
